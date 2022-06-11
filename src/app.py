38527b5c8bc81779ced92f175162b999bd52be57
Juandinet <juandinet@gmail.com>
2022-06-11 17:40:11 -0500

    deploy automatico_2

--- src/app.py
+++ src/app.py
@@ -10,8 +10,8 @@ app =Flask(__name__)
 
 @app.route( '/' ) # prueba de ruta
 def index():
-    numero = Decimal(1.6)
-    mensaje = f'API by Juandinet { numero }'
+    numero = Decimal(1.7)
+    mensaje = f'API by Juandinet { numero:.2f }'
     return jsonify( { f'Message': mensaje} ), 200
 
 @app.route( '/createTable' ) # crear tabla
